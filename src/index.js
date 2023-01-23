7bbf65c9b533e7631f416ee8e22fa1a7f084c109
Don Nnguyen <donng24@gmail.com>
2023-01-23 12:40:49 -0600

    tweaks

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const App = () => {
         <nav className="button-container">
           <ul>
             <li>
-          <NavLink path="/" exact activeClassName="active" className="home-button">
+          <NavLink to="/" exact activeClassName="active" className="home-button">
             Home
           </NavLink>
 
@@ -38,7 +38,7 @@ const App = () => {
           </ul>
         </nav>
         <Switch>
-          <Route to="/">
+          <Route path="/">
             <Home />
           </Route>
 
